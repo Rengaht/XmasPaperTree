5ae665495378e36e009679a69fe04dee46545d74
Reng Tsai <rengah0@gmail.com>
2016-12-08 18:35:10 +0800

    v2
    - 4 songs encoded
    - with lights

--- Tone.ino
+++ Tone.ino
@@ -1,36 +1,139 @@
 
 
-void singleTone(unsigned char divisor, unsigned char octave, unsigned long duration,int light_1,int light_2,int light_3){
-  
-  
+void singleTone(int note, unsigned int duration,unsigned int rest,int light_mode){
+
+
+  unsigned char octave=(note>10)?5:4;  
+  unsigned char divisor=_freq[note];
+
+#ifdef __AVR_ATtiny85__
   TCCR1 = 0x90 | (8-octave); // for 1MHz clock
   // TCCR1 = 0x90 | (11-octave); // for 8MHz clock
   OCR1C = divisor-1;         // set the OCR
+#else
+    Serial.println(divisor);    
+#endif
+
+  int num=duration/_blink;
   
-  if(light_1==1) digitalWrite(LED_PIN_1,HIGH);
-  if(light_2==1) digitalWrite(LED_PIN_2,HIGH);
-  if(light_3==1) digitalWrite(LED_PIN_3,HIGH);
+  switch(light_mode){
+    case 1:
+      digitalWrite(LED_PIN[0],HIGH);
+      delay(duration);
+      digitalWrite(LED_PIN[0],LOW);
+//      delay(duration/2);
+      break;
+    case 2:    
+      digitalWrite(LED_PIN[1],HIGH);
+      delay(duration);
+      digitalWrite(LED_PIN[1],LOW);
+//      delay(duration/2);
+      break;
+    case 4:
+      digitalWrite(LED_PIN[2],HIGH);
+      delay(duration);
+      digitalWrite(LED_PIN[2],LOW);
+//      delay(duration/2);
+      break;
+
+
+    case 3:
+      digitalWrite(LED_PIN[0],HIGH);
+      digitalWrite(LED_PIN[1],HIGH);
+      delay(duration);
+      digitalWrite(LED_PIN[0],LOW);
+      digitalWrite(LED_PIN[1],LOW);
+//      delay(duration/2);
+      break;
+   case 6:
+      digitalWrite(LED_PIN[1],HIGH);
+      digitalWrite(LED_PIN[2],HIGH);
+      delay(duration);
+      digitalWrite(LED_PIN[1],LOW);
+      digitalWrite(LED_PIN[2],LOW);
+//      delay(duration/2);
+      break;
+   case 5:
+      digitalWrite(LED_PIN[0],HIGH);
+      digitalWrite(LED_PIN[2],HIGH);
+      delay(duration);
+      digitalWrite(LED_PIN[0],LOW);
+      digitalWrite(LED_PIN[2],LOW);
+//      delay(duration/2);
+      break;
+   
+    
+    case 7:
+      digitalWrite(LED_PIN[0],HIGH);
+      digitalWrite(LED_PIN[1],HIGH);
+      digitalWrite(LED_PIN[2],HIGH);
+      delay(duration);
+      digitalWrite(LED_PIN[0],LOW);
+      digitalWrite(LED_PIN[1],LOW);
+      digitalWrite(LED_PIN[2],LOW);
+//      delay(duration/2);      
+      break;
+    case 8:
+      for(int i=0;i<num;++i){      
+        digitalWrite(LED_PIN[0],HIGH);
+        digitalWrite(LED_PIN[1],HIGH);
+        digitalWrite(LED_PIN[2],HIGH);
+        delay(_blink/2);
+        digitalWrite(LED_PIN[0],LOW);
+        digitalWrite(LED_PIN[1],LOW);
+        digitalWrite(LED_PIN[2],LOW);
+        delay(_blink/2);      
+      }
+      break;
+  }
  
-  delay(duration);
   
-  if(light_1==1) digitalWrite(LED_PIN_1,LOW);
-  if(light_2==1) digitalWrite(LED_PIN_2,LOW);
-  if(light_3==1) digitalWrite(LED_PIN_3,LOW);
   
+#ifdef __AVR_ATtiny85__
   TCCR1 = 0x90;              // stop the counter
+#endif
+  
+  delay(rest);
   
 }
 
-void playTone(){
-  
-  singleTone(NOTE_C, 4, 500, 1,0,0);
-  singleTone(NOTE_D, 4, 500, 0,1,0);
-  singleTone(NOTE_E, 4, 500, 0,0,1);
-  singleTone(NOTE_F, 4, 500, 0,1,0);
-  singleTone(NOTE_G, 4, 500, 1,0,0);
-  singleTone(NOTE_A, 4, 500, 0,1,0);
-  singleTone(NOTE_B, 4, 500, 0,0,1);
-  singleTone(NOTE_C, 5, 500, 1,1,1);  
-  
+void playSong(){
+    
+    int len_=0;
+    
+    switch(_song_index){
+      case 0:      
+        len_=27;
+        for(int i=0;i<len_;++i){
+          singleTone(int(_song_1[i]&0xF0)/16,int(_due_1[i]&0xF0)/16*_step,_rest,int(_light_1[i]&0xF0)/16);      
+          singleTone(int(_song_1[i]&0x0F),int(_due_1[i]&0x0F)*_step,_rest,int(_light_1[i]&0x0F));      
+        } 
+        break;
+      case 1:
+        len_=26;
+        for(int i=0;i<len_;++i){
+          singleTone(int(_song_2[i]&0xF0)/16,int(_due_2[i]&0xF0)/16*_step,_rest,int(_light_2[i]&0xF0)/16);      
+          singleTone(int(_song_2[i]&0x0F),int(_due_2[i]&0x0F)*_step,_rest,int(_light_2[i]&0x0F));      
+        }
+        break;
+      case 2:
+        len_=29;
+        for(int i=0;i<len_;++i){
+            singleTone(int(_song_3[i]&0xF0)/16,int(_due_3[i]&0xF0)/16*_step,_rest,int(_light_3[i]&0xF0)/16);      
+            singleTone(int(_song_3[i]&0x0F),int(_due_3[i]&0x0F)*_step,_rest,int(_light_3[i]&0x0F));  
+        }
+        break; 
+     case 3:
+        len_=55;
+        for(int i=0;i<len_;++i){
+            singleTone(int(_song_4[i]&0xF0)/16,int(_due_4[i]&0xF0)/16*_step,_rest,int(_light_4[i]&0xF0)/16);      
+            singleTone(int(_song_4[i]&0x0F),int(_due_4[i]&0x0F)*_step,_rest,int(_light_4[i]&0x0F));  
+        }
+        break;
+    }
+    delay(100);
+
+    _song_index=(_song_index+1)%MSONG;
+    if(_song_index==MSONG-1) shuffleSongOrder();
 }
 
--- XmasPaperTree.ino
+++ XmasPaperTree.ino
@@ -1,51 +1,103 @@
+
+#include "song_1.h"
+#include "song_2.h"
+#include "song_3.h"
+#include "song_4.h"
+
+
 #define SPEAKER_PIN 1
 #define SENSOR_PIN 0
 
-#define LED_PIN_1 2
-#define LED_PIN_2 3
-#define LED_PIN_3 4
+#define MSONG 4
+#define SENSOR_DELAY 3000
 
-#define NOTE_C  239
-#define NOTE_CS 225
-#define NOTE_D  213
-#define NOTE_DS 201
-#define NOTE_E  190
-#define NOTE_F  179
-#define NOTE_FS 169
-#define NOTE_G  159
-#define NOTE_GS 150
-#define NOTE_A  142
-#define NOTE_AS 134
-#define NOTE_B 127
+int LED_PIN[]={4,3,2};
 
-bool _sing=false;
-bool _water=false;
+int _step=200;
+int _rest=120;
+int _blink=20;
+
+
+int _song_order[]={1,2,3,4};
+int _song_index=0;
+
+unsigned char _freq[]={239,  213,  201,  190,
+                      179,  169,  159,  150, 
+                      142,  134,  127,  239,  
+                      225,  213,  201,  159};
+
+int _sensor_stage=0; // 0: null, 1: water, 2: dry, 3:sing
+
+
+unsigned long _sensed_time;
 
 void setup(){
 
   pinMode(SPEAKER_PIN,OUTPUT);
-  pinMode(LED_PIN_1,OUTPUT);
-  pinMode(LED_PIN_2,OUTPUT);
-  pinMode(LED_PIN_3,OUTPUT);
+  pinMode(LED_PIN[0],OUTPUT);
+  pinMode(LED_PIN[1],OUTPUT);
+  pinMode(LED_PIN[2],OUTPUT);
 
   pinMode(SENSOR_PIN,INPUT);
 
-  
+#ifndef __AVR_ATtiny85__
+  Serial.begin(9600);
+#endif
+
+  shuffleSongOrder();
+  _sensor_stage=0;  
   
 }
 
 void loop(){
+  
+  bool _high=(digitalRead(SENSOR_PIN)==HIGH);
 
-  if(!_sing){
-    bool _high=(digitalRead(SENSOR_PIN)==HIGH);
-    if(!_water && _high){
-      _water=true;    
-      return;
-    }
-    if(_water && !_high) _sing=true;
-  }else{
-    playTone();
-    delay(500);    
+
+  switch(_sensor_stage){
+     case 0: //null
+        if(_high){
+          _sensor_stage=1;
+          _sensed_time=millis();
+#ifndef __AVR_ATtiny85__
+          Serial.println("water!");
+#endif
+        }
+        break;
+     case 1: //water
+        if(!_high && millis()-_sensed_time>SENSOR_DELAY){
+            _sensor_stage=2;
+            _sensed_time=millis();
+#ifndef __AVR_ATtiny85__
+            Serial.println("dry!");
+#endif          
+        }
+        break;
+     case 2: //dry
+        if(millis()-_sensed_time>SENSOR_DELAY){
+            _sensor_stage=3;
+#ifndef __AVR_ATtiny85__
+          Serial.println("sing!");
+#endif
+        }
+        break;
+     case 3: //sing
+        playSong();
+
+        _step=constrain(_step-10,80,240);
+        _rest=_step/2;
+        break;
   }
   
 }
+
+void shuffleSongOrder(){
+
+  for(int a=0;a<MSONG;a++){
+    int r=random(a,MSONG);
+    int temp=_song_order[a];
+    _song_order[a]=_song_order[r];
+    _song_order[r]=temp;
+  }
+}
+
